16a43b6b257d29292e415dd1e3fa2758715c7e76
Danilo Silva <danilo.silva@msn.com>
2023-01-27 22:43:27 -0300

    Organizando a Solução

---
+++ PagamentoContext/PagamentoContext.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PagamentoContext.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
